859f076173c078e1e9a4f716d62e9b82eaf1ec4b
Your Name <you@example.com>
2024-08-26 17:44:28 +0530

    updated

--- components/text1.tsx
+++ components/text1.tsx
@@ -4,8 +4,8 @@ export default function text1() {
   return (
     <div className="p-4 border rounded shadow-md">
       <h2 className="text-2xl font-bold mb-2 text-black leading-tight text-center italic">
-        It's been 25 incredible years since our parents Nagesh and Jaya Started
-        their journey together
+        It&#39;s been 25 incredible years since our parents Nagesh and Jaya
+        started their journey together
       </h2>
     </div>
   );
